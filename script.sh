8c40cc720f45504f1861d3ae080d53de74d89df3
suryatej01 <chatlasuryateja333@gamil.com>
2026-05-02 18:40:09 +0530

    my-commit

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 
-if [$USERID -ne 0] then
+if [$USERID -ne 0]; then
 	echo "Run the script as a root user"
 	exit 1  	# failure is other tjhan 0	
 fi
@@ -10,22 +10,19 @@ fi
 
 dnf install mysql -y
  
-if [$? -ne 0] then
+if [$? -ne 0]; then
 	echo "Error: my sql installaing is failure"
-	exit 1  	# failure is other tjhan 0	
 else 
 	echo "mysql installation is success" 
-
 fi
 
 dnf install nginx -y
  
-if [$? -ne 0] then
+if [$? -ne 0]; then
 	echo "Error: my nginx installaing is failure"
 	exit 1  	# failure is other than 0	
 else 
 	echo "nginx installation is success" 
-
 fi 
 
 # $? fecth the last exit code 
